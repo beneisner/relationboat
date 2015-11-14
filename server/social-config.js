777613973e2a1836f6cd47fbe57202349f8faf9a
beneisner <beisner@princeton.edu>
2015-11-14 17:40:24 -0500

    added a login feature, as well as a complete to-do app

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1647488272167372',
+    secret: '7a2c3117f8f54dbfcadcaea44b00ed85'
+});
