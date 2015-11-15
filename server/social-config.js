e76df160f205902a2722607c3b73b409a3074d65
beneisner <beisner@princeton.edu>
2015-11-14 20:49:47 -0500

    Fixed some publish-subscribe things

--- server/social-config.js
+++ server/social-config.js
@@ -7,3 +7,9 @@ ServiceConfiguration.configurations.insert({
     appId: '1647488272167372',
     secret: '7a2c3117f8f54dbfcadcaea44b00ed85'
 });
+
+Meteor.publish('userData', function () {
+	return Meteor.users.find({_id: this.userId},
+		{fields: {'services': 1 }
+	});
+});
